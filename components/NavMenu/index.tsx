5ede40db7a0eb02cb7874a6d45e579fbdf7639df
TracyMuso <musongoletracy@gmail.com>
2024-05-20 16:28:24 +0200

    feat(nav-menu): update  styles for nav-menu on landing page

--- components/NavMenu/index.tsx
+++ components/NavMenu/index.tsx
@@ -1,10 +1,30 @@
 import Image from "next/image";
+import Link from "next/link";
+import { navLinks } from "@/constants/data/navMenu";
 
-const NavMenu: React.FC = () => {
+const NavMenu = () => {
   return (
-    <div className="py-5 px-16 flex space-between w-full">
+    <div className="py-4 px-8 flex justify-between items-center m-0 w-full font-Montserrat">
       <div className="logo p-0">
-        <Image src="/Images/Logo.png" alt="logo" width={7} height={4} />
+        <Link href={"/"}>
+          <Image src="/Images/Logo.png" alt="logo" width={180} height={100} />
+        </Link>
+      </div>
+      <div className="flex justify-between gap-4">
+        {navLinks.map((item, idx) => (
+          <Link
+            className="hover:underline hover:decoration-4 hover:decoration-purple-primary"
+            href={item.url}
+            key={idx}
+          >
+            {item.title}
+          </Link>
+        ))}
+      </div>
+      <div>
+        <button className="px-8 py-3 text-center bg-purple-500 hover:bg-purple-primary rounded-md text-white">
+          Sign up
+        </button>
       </div>
     </div>
   );
---
+++ constants/data/navMenu.ts
@@ -0,0 +1,23 @@
+export interface NavigationLink {
+  title: string;
+  url: string;
+}
+
+export const navLinks: NavigationLink[] = [
+  {
+    title: "Home",
+    url: "/",
+  },
+  {
+    title: "About Us",
+    url: "/about-us",
+  },
+  {
+    title: "Our Product",
+    url: "/product",
+  },
+  {
+    title: "Blog and News",
+    url: "/blog",
+  },
+];
