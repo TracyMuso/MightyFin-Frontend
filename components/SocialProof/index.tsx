c7228b8f8f7a9ef6e4d2f5cde782835006acc5b2
TracyMuso <musongoletracy@gmail.com>
2024-05-22 15:29:52 +0200

    feat(social-proof-section): style the right text and image on the social proof section on the landing page

--- components/SocialProof/index.tsx
+++ components/SocialProof/index.tsx
@@ -1,9 +1,16 @@
+import styles from "../../styles/socialProof.module.css";
+
 const SocialProof = () => {
   return (
-    <section className="font-Montserrat flex flex-col bg-yellow-300">
-      <div className="flex justify-between">
-        <div className="right-text">
-          <h3 className="font-700 font-bold leading-6"></h3>
+    <section className="font-Montserrat flex flex-col">
+      <div className="flex justify-between gap-4 bg-yellow-300 h-[255px] w-full">
+        <div className={`relative w-2/5`}>
+          <div className={`${styles.rightText}`} />
+          <div className="absolute top-[25%] left-[12%] border-left-2 pr-[63px]">
+            <h3 className="font-700 font-extrabold text-3xl leading-14 text-left">
+              Trusted By Thousands Real People, Real Results
+            </h3>
+          </div>
         </div>
         <div>
           <span>
