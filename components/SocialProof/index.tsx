63bf5357ea8b1104ce0128dcca35436ad624c2a1
TracyMuso <musongoletracy@gmail.com>
2024-05-22 14:27:51 +0200

    feat(social-proof-section): create skeleton of social proof and leadership recognition section on the landing page

---
+++ components/SocialProof/index.tsx
@@ -0,0 +1,38 @@
+const SocialProof = () => {
+  return (
+    <section className="font-Montserrat flex flex-col bg-yellow-300">
+      <div className="flex justify-between">
+        <div className="right-text">
+          <h3 className="font-700 font-bold leading-6"></h3>
+        </div>
+        <div>
+          <span>
+            <p>3 years</p>
+            <small>experience</small>
+          </span>
+          <span>
+            <p>Over K 5M</p>
+            <small>loan disbursed</small>
+          </span>
+          <span>
+            <p>3.5 k </p>
+            <small>registered users</small>
+          </span>
+        </div>
+      </div>
+      <div>
+        <div>
+          <h2>Empowering You with Strong Partnerships</h2>
+          <p>
+            We believe in collaboration and excellence. Discover our trusted
+            partners and the awards recognizing our achievements.
+          </p>
+        </div>
+        <div>icons</div>
+      </div>
+      <div>Leadership Recognition</div>
+    </section>
+  );
+};
+
+export default SocialProof;
