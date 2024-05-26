f21092303581f04b57e401cf275bb244e8ff32bf
TracyMuso <musongoletracy@gmail.com>
2024-05-26 11:47:53 +0200

    feat(hero-section): style the main text on the hero section on the landing page

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -1,12 +1,15 @@
+import Image from "next/image";
+
 const Hero = () => {
   return (
-    <section className="relative py-8 px-6 ">
-      <div className="main-text ">
-        <article>
-          <h1>
-            <span>Empower</span>Your Future with Streamlined Loan Options
+    <section className="relative h-[87vh] px-6 font-Montserrat">
+      <div className="absolute main-text text-left left-[5%] top-[5%] w-1/2">
+        <article className="py-8">
+          <h1 className="text-4xl font-extrabold leading-12 pb-12 pt-6">
+            <span className="text-yellow-300">Empower </span>Your Future with
+            Streamlined Loan Options
           </h1>
-          <p>
+          <p className="w-4/5 text-gray-400">
             We are a financial technology company offering convenient and
             accessible loan solutions. We provide secure and personalized loan
             options to help you achieve your goals. Simplifying access to
@@ -15,12 +18,19 @@ const Hero = () => {
           </p>
         </article>
         <div>
-          users
-          <span>Over 3.5k active users</span>
+          <Image
+            src={"/Images/LandingPage/UserImages.png"}
+            alt="user images"
+            width={220}
+            height={60}
+          />
+          <span className="text-purple-500 font-extrabold py-6 text-xl">
+            Over 3.5k active users
+          </span>
         </div>
         <button></button>
       </div>
-      <div className="img-hover"></div>
+      <div className="img-hover absolute w-1/2 h-[503px] border border-red right-[4%] top-[4%] "></div>
     </section>
   );
 };
