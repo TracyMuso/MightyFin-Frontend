cf9a181608958900a89d52a4648bd1eeca040268
TracyMuso <musongoletracy@gmail.com>
2024-05-27 16:22:09 +0200

    feat(about-us): create the skeleton of the about us section on the landing page

---
+++ components/AboutUs/index.tsx
@@ -0,0 +1,21 @@
+const AboutUs = () => {
+  return (
+    <section>
+      <div>picture frame</div>
+      <div>
+        <span>
+          We simplify access to loans, empowering you to achieve your goals.
+        </span>
+        <p>
+          At Mighty Fin, we believe everyone deserves access to convenient and
+          flexible financing solutions. We leverage technology to create a
+          seamless loan application process, with competitive rates and
+          transparent terms.
+        </p>
+        <button>Learn More</button>
+      </div>
+    </section>
+  );
+};
+
+export default AboutUs;
