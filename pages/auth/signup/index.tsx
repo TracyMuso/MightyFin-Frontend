e2525a3c3126fb8c645e3b6e7c353d842d5f18e6
TracyMuso <musongoletracy@gmail.com>
2024-06-04 10:48:15 +0200

    feat(signup): style the main picture and signup form on the user signup page

--- pages/auth/signup/index.tsx
+++ pages/auth/signup/index.tsx
@@ -1,31 +1,45 @@
 import Image from "next/image";
 import { Input } from "@/components/Input";
+import { signUpDets } from "@/constants/data/authPages";
 import styles from "../../../styles/userAuth.module.css";
 
 const Signup = () => {
   return (
-    <section className="flex">
-      <div className={`${styles.leftCont} relative w-1/2 h-[100vh]`}>
-        <div>
+    <section className="flex p-0 h-[100vh] font-Montserrat">
+      <div className={`${styles.leftCont} relative`}>
+        <div className="text-left absolute top-[34px] left-[35px]">
           <Image
             src={"/Icons/mf-logo.png"}
             width={89}
             height={59}
             alt="mighty finance logo"
           />
-          <p>Financial inclusion for all</p>
+          <p className="text-white">Financial inclusion for all</p>
         </div>
-        <div>
-          <div>Look out for the reminders so you pay on time</div>
+        <div className="absolute top-[270px] left-7">
+          <div
+            className={`${styles.phoneNotification} flex justify-center rounded-md w-[207px] h-[70px] py-2 px-3 text-sm text-white`}
+          >
+            <p className="leading-6">
+              Look out for the reminders so you pay on time
+            </p>
+          </div>
           <Image
             src={"/Icons/Polygon-2.png"}
             width={34}
             height={34}
             alt="small polygon"
+            className="ml-auto"
           />
         </div>
-        <div>
-          <div>Dear customer, your loan has been approved!</div>
+        <div className="absolute top-[360px] left-72">
+          <div
+            className={`${styles.phoneNotification} flex justify-center rounded-md w-[207px] h-[70px] py-2 px-3 text-sm text-white`}
+          >
+            <p className="leading-6">
+              Dear customer, your loan has been approved!
+            </p>
+          </div>
           <Image
             src={"/Icons/Polygon-2.png"}
             width={34}
@@ -34,38 +48,30 @@ const Signup = () => {
           />
         </div>
       </div>
-      <div>
-        <div>
-          <p>Create an Account</p>
-          <p>Welcome to Mighty Finance. Create your account below.</p>
+      <div className="flex flex-col mx-auto w-1/2">
+        <div className="flex flex-col items-center py-3 gap-1">
+          <p className="font-bold text-purple-200 text-center">
+            Create an Account
+          </p>
+          <p className="text-m">
+            Welcome to Mighty Finance. Create your account below.
+          </p>
         </div>
         <div>
           <form>
-            <div>
-              <div>
-                <label>First Name</label>
-                <Input type="text" placeholder="" />
-              </div>
-              <div>
-                <label>Last Name</label>
-                <Input type="text" placeholder="" />
-              </div>
-            </div>
-            <div>
-              <label>Email Address</label>
-              <Input type="text" placeholder="" />
-            </div>
-            <div>
-              <label>Phone Number</label>
-              <Input type="text" placeholder="" />
-            </div>
-            <div>
-              <label>Password</label>
-              <Input type="text" placeholder="" />
+            <div className="grid grid-cols-2 gap-4">
+              {signUpDets.map((item, idx) => (
+                <div key={idx} className="w-full">
+                  <label htmlFor={item.htmlFor} className="font-bold text-m">
+                    {item.label}
+                  </label>
+                  <Input type={item.inputType} placeholder="" id={item.id} />
+                </div>
+              ))}
             </div>
             <div>
               <label>Stay signed in</label>
-              <Input type="text" placeholder="" />
+              <input type="checkbox" placeholder="" />
             </div>
             <button>Create account</button>
           </form>
