33905a7ab02beefd8500d616133dcb6472edc172
TracyMuso <musongoletracy@gmail.com>
2024-06-06 09:45:31 +0200

    feat(signup): complete styling the signup form on the user signup page

--- pages/auth/signup/index.tsx
+++ pages/auth/signup/index.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import { Input } from "@/components/Input";
 import { signUpDets } from "@/constants/data/authPages";
 import styles from "../../../styles/userAuth.module.css";
@@ -48,7 +49,7 @@ const Signup = () => {
           />
         </div>
       </div>
-      <div className="flex flex-col mx-auto w-1/2">
+      <div className="flex flex-col justify-center mx-auto w-[50%] px-5">
         <div className="flex flex-col items-center py-3 gap-1">
           <p className="font-bold text-purple-200 text-center">
             Create an Account
@@ -57,11 +58,11 @@ const Signup = () => {
             Welcome to Mighty Finance. Create your account below.
           </p>
         </div>
-        <div>
+        <div className="flex flex-col justify-center">
           <form>
             <div className="grid grid-cols-2 gap-4">
               {signUpDets.map((item, idx) => (
-                <div key={idx} className="w-full">
+                <div key={idx} className="w-full py-1">
                   <label htmlFor={item.htmlFor} className="font-bold text-m">
                     {item.label}
                   </label>
@@ -69,42 +70,65 @@ const Signup = () => {
                 </div>
               ))}
             </div>
-            <div>
-              <label>Stay signed in</label>
-              <input type="checkbox" placeholder="" />
-            </div>
-            <button>Create account</button>
+            <div className="w-full py-2"> </div>
+            <button className="w-full py-2 text-center bg-purple-500 hover:bg-purple-primary rounded text-white font-bold">
+              Create account
+            </button>
           </form>
-          <div>
-            <div>
-              <span />
-              <p>or</p>
-              <span />
+          <small className="py-2 mx-auto w-1/3">
+            Already have an account?{" "}
+            <Link href={"/auth/sign-in"} className="text-yellow-400">
+              Log In
+            </Link>
+          </small>
+          <div className="pb-2">
+            <div className="flex justify-between items-center pb-3">
+              <span className={`${styles.gradDiv}`} />
+              <p className="text-sm">or</p>
+              <span className={`${styles.gradDiv}`} />
             </div>
-            <div>
-              <div className="bg-[#FAF1FF]">
+            <div className="flex items-center justify-between gap-3 w-4/5 mx-auto">
+              <div className="bg-[#FAF1FF] flex justify-center items-center gap-3 py-2 px-5 rounded">
                 <Image
                   src={"/Icons/google.png"}
                   alt="google-icon"
-                  width={35}
-                  height={35}
+                  width={25}
+                  height={25}
                 />
-                <p>Continue with Google</p>
+                <p className="text-sm">Continue with Google</p>
               </div>
-              <div className="bg-[#FAF1FF]">
+              <div className="bg-[#FAF1FF] flex justify-center items-center gap-3 py-2 px-5 rounded">
                 <Image
                   src={"/Icons/apple.png"}
                   alt="apple-icon"
-                  width={35}
-                  height={35}
+                  width={25}
+                  height={25}
                 />
-                <p>Continue with Apple</p>
+                <p className="text-sm">Continue with Apple</p>
               </div>
             </div>
             <div>
-              <small>
-                Already have an account? <span>Log In</span>
-              </small>
+              <div className="pt-5">
+                <label htmlFor="user-agreement" className="text-m">
+                  <input
+                    className="w-4 h-4 text-[#CFD3D4]  
+                          focus:ring-purple-200 focus:ring-2 mr-1"
+                    type="checkbox"
+                    id="user-agreement"
+                  />
+                  By creating an account, you agree to Mighty Finance Solution
+                  <Link href={"#"} className="text-yellow-300">
+                    {" "}
+                    Terms of Services
+                  </Link>{" "}
+                  and{" "}
+                  <Link href={"#"} className="text-yellow-300">
+                    {" "}
+                    Privacy Policy
+                  </Link>
+                  .
+                </label>
+              </div>
             </div>
           </div>
         </div>
