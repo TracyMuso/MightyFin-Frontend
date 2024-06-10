00c2064fd128f8620d19cb8b0038e185a29bd762
TracyMuso <musongoletracy@gmail.com>
2024-06-10 15:01:32 +0200

    feat(left-sidebar): create dashboard layout on dashboard home page

---
+++ constants/data/dashboard.ts
@@ -0,0 +1,46 @@
+import { type sidebarDataType } from "@/types";
+
+export const LeftSidebarData: sidebarDataType[] = [
+  {
+    icon: "/Icons/Category.png",
+    alt: "Dashboard icon",
+    title: "Dashboard",
+    onClick: () => {},
+  },
+  {
+    icon: "/Icons/Notifications.png",
+    alt: "Notifications icon",
+    title: "Notifications",
+    onClick: () => {},
+  },
+  {
+    icon: "/Icons/fi-rr-document.png",
+    alt: "Document icon",
+    title: "Your Documents",
+    onClick: () => {},
+  },
+  {
+    icon: "/Icons/Loan-History.png",
+    alt: "Chart icon",
+    title: "Transactions",
+    onClick: () => {},
+  },
+  {
+    icon: "/Icons/fi_headphones.png",
+    alt: "headphones icon",
+    title: "Customer Support",
+    onClick: () => {},
+  },
+  {
+    icon: "/Icons/Settings.png",
+    alt: "Settings icon",
+    title: "Settings",
+    onClick: () => {},
+  },
+  {
+    icon: "/Icons/Sign-out.png",
+    alt: "arrow left icon",
+    title: "Log out",
+    onClick: () => {},
+  },
+];
---
+++ pages/dashboard/index.tsx
@@ -0,0 +1,11 @@
+import DashboardLayout from "@/components/Layouts/Dashboard";
+
+const Dashboard = () => {
+  return (
+    <DashboardLayout>
+      <div>hi</div>
+    </DashboardLayout>
+  );
+};
+
+export default Dashboard;
