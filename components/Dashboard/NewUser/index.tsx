4e645d02643e302bec6dec49d9e5ed964661e668
TracyMuso <musongoletracy@gmail.com>
2024-06-16 23:42:50 +0200

    feat(dashboard-main-no-loan): update styles of left side-bar

--- components/Dashboard/NewUser/index.tsx
+++ components/Dashboard/NewUser/index.tsx
@@ -1,10 +1,12 @@
 import styles from "../../../styles/Dashboard.module.css";
 import CenterBar from "./centerBar";
+import RightSideBar from "./rightSidebar";
 
 const NewUserPage = () => {
   return (
     <div className={`${styles.main} grid grid-cols-[850px_1fr] gap-1`}>
       <CenterBar />
+      <RightSideBar />
     </div>
   );
 };
