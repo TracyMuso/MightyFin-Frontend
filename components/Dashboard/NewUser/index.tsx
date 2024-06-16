1612fd066d4635f2153643b95b8e111a90f8c4d7
TracyMuso <musongoletracy@gmail.com>
2024-06-16 23:14:08 +0200

    feat(dashboard-main-no-loan): create menu and cta buttons on right side-bar

--- components/Dashboard/NewUser/index.tsx
+++ components/Dashboard/NewUser/index.tsx
@@ -1,8 +1,11 @@
 import styles from "../../../styles/Dashboard.module.css";
+import CenterBar from "./centerBar";
 
 const NewUserPage = () => {
   return (
-    <div className={`${styles.main} grid grid-cols-[850px_1fr] gap-1`}></div>
+    <div className={`${styles.main} grid grid-cols-[850px_1fr] gap-1`}>
+      <CenterBar />
+    </div>
   );
 };
 // This is the tab for a brand new user with no loan, transactions, profile pic or doc uploads
